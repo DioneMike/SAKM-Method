711c4948b4691c7a96a1d1e780f9dafe20d35ad8
DioneMike <micheldione97@gmail.com>
2023-06-27 15:11:21 +0200

    Contients toutes fonction utilisées dans le SAKM

---
+++ MesFonctions/KernelUpdate_NormTruncSGradient.py
@@ -0,0 +1,123 @@
+import numpy as np
+import pandas as pd
+from Kernel_Machine_Initialisation import Kernel_Machine_Initialisation
+import math
+
+
+def KernelUpdate_NormTruncSGradient(WinKern,Xnew,Gamma,nu,eta,Kard,KM):
+
+    # global KM
+    
+    if len(KM) == 0 :
+        print("Il n'y a aucun noyau à mettre à jour !")
+        return
+        
+    else :
+        
+        WKern = KM[int(WinKern[0][0])] # le noyau gagnant
+        
+        
+        
+        Smval = WinKern[0][1] # la valeur de similarité du noyau gagnant (ou le cluster gagnant)
+        
+        #--------------------------  Processus de mis à jour du noyau gagnant ----------------------------------------------#
+    
+        if Smval < 0: # cas de vecteur de support éléments d'erreur de marge (=> delta = 1 => muPhi non nul(voir page 5) => il y a similarité)
+            
+            
+            
+            WKern["Xsv"] = np.append(WKern["Xsv"],Xnew, axis=0) # mis à jour des vecteurs support
+            
+            WKern["wgh"] = np.append((1-eta)*WKern["wgh"], eta) # mis à jour des poids ; ajouter eta à la liste (1-eta)*Wkern.Wgh
+            
+            WKern["Rho"] = WKern["Rho"] + eta*(1-nu) # mis à jour du décalage 
+            
+            WKern["idsv"] = np.append(WKern["idsv"] , WKern["data"].shape[0]) # mis à jour des indices de SV
+            
+            WKern["data"] = np.append(WKern["data"], Xnew, axis=0) # mis à jour des données du noyau
+        else :
+            WKern["wgh"] = (1-eta)*WKern["wgh"]
+            WKern["Rho"] =  WKern["Rho"] - eta*nu
+            
+            WKern["data"] = np.append(WKern["data"], Xnew,axis=0)
+            
+    
+        #---------------------------------------- Troncature ------------------------------------------------------------------#
+    
+        if WKern["Xsv"].shape[0] == Kard :
+            
+           WKern["data"]= np.delete(WKern["data"], 0, axis=0)
+           
+           
+        if WKern["Xsv"].shape[0] > Kard :
+            iw = 0
+            WKern["data"]= np.delete(WKern["data"], iw, axis=0)
+            WKern["Xsv"] = np.delete(WKern["Xsv"], iw, axis=0)
+            WKern["wgh"] =np.delete(WKern["wgh"], iw, axis=0)
+            WKern["idsv"] =np.delete(WKern["idsv"], iw, axis=0)
+            
+     
+          
+        #----------------------------------------Condition suplémentaire: sum(alpha_i) = 1 et Rho = f(Xsvi)  --------------------#
+    
+        ts =  max(math.floor(WKern["wgh"].shape[0] / 2), 0)-1
+       
+        WKern["wgh"] = WKern["wgh"]/np.sum(WKern["wgh"]) # normalisation des poids alpha
+        
+       
+        MXsv = np.ones((WKern["wgh"].shape[0], 1)).dot((WKern["Xsv"][ts,:]).reshape(1,-1))
+        M = (WKern["Xsv"]-MXsv).dot((WKern["Xsv"]-MXsv).T)
+        
+        dist = np.diag(M)
+        ksvi = np.exp(-Gamma*dist)
+        WKern["Rho"] = (WKern["wgh"]).T.dot(ksvi)
+        KernUpdat = WKern
+
+    return KernUpdat
+
+
+# data = pd.read_csv('Env_nonstat.csv', sep='\t')
+# xo = np.array(data)[1:2595,0]
+# X =np.insert(np.array(data)[1:2595,0:2],1,xo,axis=1)
+# Gamma = 0.1
+
+# X = pd.read_csv('datatest.csv',sep=";", header=None)
+# X = np.array(X)
+# X1 = X[0,:].reshape(1,-1)
+# X2 = X[102,:].reshape(1,-1)
+
+# gamma = 2
+# nu = 0.3
+# eta = 0.2
+# kard = 50
+# ThSim = 50
+# Nc = 10; T = 30
+# # # global KM
+# # # KM = []
+
+# # # print(X[1,:].reshape(-1,1).shape)
+# # KM = Kernel_Machine_Initialisation(X, Gamma, nu, eta,[])
+# # K = KernelUpdate_NormTruncSGradient([[0,0.2]],X,Gamma,nu,eta,kard,KM)
+# Xini = X[2,:].reshape(1,-1)
+# KM = Kernel_Machine_Initialisation(X1, gamma, nu, eta,[])
+# from Kernel_Decision_Function import Kernel_Decision_Function
+# w_k = Kernel_Decision_Function(X2, ThSim, gamma,KM)
+
+# print(w_k)
+# # # #print(.shape)
+
+# K = KernelUpdate_NormTruncSGradient(w_k,X2,gamma,nu,eta,kard,KM)
+
+# print(K)
+# # # # # print(K["Xsv"].shape)
+# # # # # print(K["data"].shape)
+
+
+# # # # K = []
+# # # # for i in range(30) :
+
+# # # # # # KERN = Kernel_Machine_Initialisation(X, Gamma, nu, eta)
+    
+# # # #     K.append(KernelUpdate_NormTruncSGradient([[0,-0.2]],X[i,:].reshape(1,-1),Gamma,nu,eta,kard,KM))
+    
+# # # # print(np.sum(K[0]["wgh"]))
---
+++ MesFonctions/Main_SelfAdaptive_KM.py
@@ -0,0 +1,164 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from Kernel_Decision_Function import Kernel_Decision_Function
+from Online_Learning_Kernel import Online_Learning_Kernel
+from Kernel_Machine_Initialisation import Kernel_Machine_Initialisation
+from Eliminate_Noise_Clusters import Eliminate_Noise_Clusters
+# from Plotting_Kernel_Clusters import Plotting_Kernel_Clusters
+# from Plotting_Kernel_Function import Plotting_Kernel_Function
+from Robust_Kernel_Machine import Robust_Kernel_Machine
+
+# Charger le fichier .mat
+# data = pd.read_csv('Env_nonstat.csv', sep='\t')
+
+# xo = np.array(data)[1:2595,0]
+# X =np.insert(np.array(data)[1:2595,0:2],1,xo,axis=1)
+
+
+import matplotlib.pyplot as plt
+# from sklearn.datasets import load_digits
+# digits = load_digits()
+# X = digits.data
+# # plt.plot(,,X[:,2])
+data = pd.read_csv('datatst.csv',sep=",", header=None)
+X = np.array(data)
+x = X[:,0]
+y = X[:,1]
+z = X[:,2]
+
+df = pd.DataFrame(data)
+
+data=np.array(X)
+# data= data[0:2595,0:2]#sio.loadmat(r'D:\Travaux\PhD_PROJECT\IMPLEMENTATIONS_CODES\DEVELOPPEMENTS\ADEME\ADEME Experiments\ADEME_Static_FN-EC-RE-FI_14-12-04.mat')
+#X = data
+#print(type(X))
+
+#X = X[:-131, :]  # Supprimer les 130 dernières lignes de X
+#print(X[:,0].shape)
+
+# X = np.delete(X, 2,axis = 1)
+
+# Afficher un graphique 3D
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.scatter(y,x,z, c='k', marker='x')
+# # ax.scatter(X[:, 0], X[:, 1], c='k', marker='x')
+# plt.show()
+
+# Mesure de Performances en dynamique
+global Rcum
+Rcum = [0]
+
+# global KM
+
+# print(X.shape)
+
+Xcl = np.empty((0,X.shape[1]))
+gamma = 2
+nu = 0.3
+eta = 0.2
+kard = 50
+ThSim = 0.55
+Nc = 10
+T = 30
+
+# Paramètres pour le graphique
+echAxes = [-2, 10, -8, 12]
+IT = 1
+Step = 10
+k = 1
+Ndat = X.shape[0]
+Axs = [0, 1]
+Data = X.copy()
+i = T
+
+# Initialisation de Kernel Machine
+global KM
+KM = Kernel_Machine_Initialisation(X[0,:].reshape(1,-1), gamma, nu, eta,[])
+
+
+
+while Data.shape[0] > 0:
+    # Acquisition de données en ligne
+    Xnew = Data[0, :].reshape(1, -1)
+    # print(k,Xnew)
+    # Data = Data[1:, :].reshape(1, -1)
+    
+    Data = np.delete(Data, 0, axis=0)
+    
+    # Fonction de décision pour Kernel Machine
+    WK_Sim = Kernel_Decision_Function(Xnew, ThSim, gamma,KM)  # À implémenter
+    
+   
+    # Apprentissage en ligne avec Kernel Machine : Initialisation du noyau et adaptation
+    Process = Online_Learning_Kernel(Xnew, WK_Sim, gamma, nu, eta, kard)  # À implémenter
+
+   
+    
+    # Machine à noyau robuste : Fusion du noyau
+    
+    Robust_Kernel_Machine(Process, WK_Sim, Xnew, gamma, nu, eta, kard,KM)  # À implémenter
+
+    # Élimination des clusters de noyau : Élimination du bruit
+    if (k == i or k == Ndat) and len(KM) > 0:
+        Xcl, Xrj = Eliminate_Noise_Clusters(Nc,KM)  # élimination de bruits
+        
+        i += T
+    else:
+        
+        
+        Xcl = np.append(Xcl, Xnew, axis=0) 
+        
+        
+
+
+    # Affichage des clusters de noyau (J'ai des soucis de code pour cette partie)
+    # if k == IT or k == Ndat:
+    #     Plotting_Kernel_Clusters(Xcl, KM, gamma, echAxes, Axs)  # À implémenter
+    #     plt.pause(0.2)
+    #     Plotting_Kernel_Function(Xcl, KM, gamma, echAxes, Axs)  # À implémenter
+    #     IT = k + Step
+    #     plt.pause(0.2)
+    
+    k += 1
+
+# Tracé de Rcum
+# plt.figure()
+# plt.plot(Rcum, 'r')
+# plt.show()
+
+
+# R =[(1/(len(KM) * Ndat))* r for r in Rcum]
+
+# # Tracé de la fonction objective
+# plt.figure()
+# plt.title('Fonction objective : Erreur moyenne dynamique')
+# plt.axis([0, 2000, 0, 1])
+# plt.plot(R, 'r')
+# plt.show()
+
+
+# import matplotlib.pyplot as plt
+
+# # X = pd.read_csv('datatest.csv',sep=";", header=None)
+# # X = np.array(X)
+# xk = KM[0]["data"][:,0]
+# yk = KM[0]["data"][:,1]
+# zk = KM[0]["data"][:,2]
+
+# fig = plt.figure(figsize = (10,10))
+# ax = plt.axes(projection='3d')
+# ax.grid()
+
+# ax.scatter(yk,xk,zk,c = 'r', s = 50)
+# ax.set_title('2D Scatter Plot')
+
+# # Set axes label
+# ax.set_xlabel('x', labelpad=20)
+# ax.set_ylabel('y', labelpad=20)
+# # ax.set_zlabel('z', labelpad=20)
+
+# plt.show()
+
